4b69115017f07da323f17072ab139e2ee7041fa1
Ataly <116371591+Atalyeu@users.noreply.github.com>
2025-03-28 00:32:57 +0200

    Update stdbool.h

--- include/atara/stdbool.h
+++ include/atara/stdbool.h
@@ -1,8 +1,17 @@
-#ifndef STDBOOL_H
-#define STDBOOL_H
+#ifndef _STDBOOL_H
+#define _STDBOOL_H 1
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
 
 #define bool _Bool
 #define true 1
 #define false 0
 
-#endif // STDBOOL_H
+#if defined(__cplusplus)
+} /* extern "C" */
+#endif
+
+
+#endif // _STDBOOL_H
